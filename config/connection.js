10c82ef7e028d58fc2edd365636f66c47e4fdbfb
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-05-08 11:36:20 -0700

    added JawsDB for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,24 @@
 var mysql = require('mysql');
 
 // MySQL connection
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	// Your Password
-	password: '27Kiarra!^Chapster',
-	database: 'burgers_db'
-})
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		// Your Password
+		password: '',
+		database: 'burgers_db'
+	})
+};
+	
+
 
 // Connect MySQL
 connection.connect(function(err) {
